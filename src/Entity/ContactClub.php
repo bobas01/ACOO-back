53c97ea02a4312522ab231e894959be4114568ce
Charbel SONON <charbelsnn@gmail.com>
2025-05-25 12:03:21 +0200

    add colonne instagram and facebook to contact_club table

--- src/Entity/ContactClub.php
+++ src/Entity/ContactClub.php
@@ -24,6 +24,12 @@ class ContactClub
     #[ORM\Column(length: 12)]
     private ?string $phoneNumber = null;
 
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $facebook = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $instagram = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -62,6 +68,28 @@ class ContactClub
     {
         $this->phoneNumber = $phoneNumber;
 
+        return $this;
+    }
+
+        public function getFacebook(): ?string
+    {
+        return $this->facebook;
+    }
+
+    public function setFacebook(?string $facebook): static
+    {
+        $this->facebook = $facebook;
+        return $this;
+    }
+
+    public function getInstagram(): ?string
+    {
+        return $this->instagram;
+    }
+
+    public function setInstagram(?string $instagram): static
+    {
+        $this->instagram = $instagram;
         return $this;
     }
 }
